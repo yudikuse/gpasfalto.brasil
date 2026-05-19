990408da68d000aadab1a71d5aa25bbde9062166
yudikuse <marcelokuse@gmail.com>
2026-05-19 14:16:14 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,21 @@
-import Nav            from '@/components/Nav'
-import PageIndicator  from '@/components/PageIndicator'
-import WaButton       from '@/components/WaButton'
-import HeroPanel      from '@/components/HeroPanel'
-import NumbersPanel   from '@/components/NumbersPanel'
-import ProjectPanel   from '@/components/ProjectPanel'
-import SegmentosPanel from '@/components/SegmentosPanel'
-import UsinasPanel    from '@/components/UsinasPanel'
-import ContactPanel   from '@/components/ContactPanel'
-import { site }       from '@/data/content'
+import { Hero } from '@/components/Hero'
+import { StatsBar } from '@/components/StatsBar'
+import { ServicesGrid } from '@/components/ServicesGrid'
+import { FeaturedProjects } from '@/components/FeaturedProjects'
+import { UsinasSection } from '@/components/UsinasSection'
+import { CtaBanner } from '@/components/CtaBanner'
+import { Footer } from '@/components/Footer'
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <div className="scroll-lock" style={{ height: '100svh' }}>
-      <Nav />
-      <PageIndicator />
-      <WaButton />
-      <main id="scrl" className="scroll-container">
-        <HeroPanel />
-        <NumbersPanel />
-        {site.projects.map((proj, i) => (
-          <ProjectPanel
-            key={proj.slug}
-            project={proj}
-            panelId={i === 0 ? 'obras' : proj.slug}
-          />
-        ))}
-        <SegmentosPanel />
-        <UsinasPanel />
-        <ContactPanel />
-      </main>
-    </div>
+    <>
+      <Hero />
+      <StatsBar />
+      <ServicesGrid />
+      <FeaturedProjects />
+      <UsinasSection />
+      <CtaBanner />
+      <Footer />
+    </>
   )
 }
