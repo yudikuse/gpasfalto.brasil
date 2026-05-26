7a10ebf436556d86dbb72da1a3d416674244c3c9
yudikuse <marcelokuse@gmail.com>
2026-05-26 13:51:11 -0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Hero } from '@/components/Hero'
 import { StatsBar } from '@/components/StatsBar'
 import { ServicesGrid } from '@/components/ServicesGrid'
 import { FeaturedProjects } from '@/components/FeaturedProjects'
+import { VideosGallery } from '@/components/VideosGallery'
 import { UsinasSection } from '@/components/UsinasSection'
 import { CtaBanner } from '@/components/CtaBanner'
 import { Footer } from '@/components/Footer'
@@ -13,6 +14,7 @@ export default function HomePage() {
       <StatsBar />
       <ServicesGrid />
       <FeaturedProjects />
+      <VideosGallery />
       <UsinasSection />
       <CtaBanner />
       <Footer />
